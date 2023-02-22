928d136e3816478f7a368bd1b9d73130c4f1f003
sakshamgupta04 <121110080+sakshamgupta04@users.noreply.github.com>
2023-02-22 19:23:21 +0530

    Add files via upload

---
+++ CharacterVowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+char ch;
+printf("Enter Any Character");
+scanf("%c",&ch);
+if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{
+printf("Character is vowel");
+}
+else
+{
+printf("Character is not vowel");
+}
+}
