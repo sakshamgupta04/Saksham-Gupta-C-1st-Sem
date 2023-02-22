fe4e9fba4d6b1580d3a1b6d8a5f7e347e0b5da4c
sakshamgupta04 <121110080+sakshamgupta04@users.noreply.github.com>
2023-02-22 18:46:24 +0530

    Add files via upload

---
+++ PerimeterAndAreaOfSquare.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+int side,perimeter,area;
+printf("Enter side of sqaure");
+scanf("%d",&side);
+perimeter=4*side;
+printf("Perimeter of square is %d",perimeter);
+area=side*side;
+printf("Area of square is %d",area);
+}
